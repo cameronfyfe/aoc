aa7bfbe2ad589cac3de8c0287f364968a4f66098
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2021-01-11 10:56:49 -0700

    fix day2

--- 2015/rust/day2/src/main.rs
+++ 2015/rust/day2/src/main.rs
@@ -1,5 +1,38 @@
 use std::fs;
 
+fn part_1(input: &Vec<Vec<u32>>) {
+    let mut amount_of_paper = 0 as u32;
+
+    for present in input {
+        let mut sides = present.clone();
+        sides.sort();
+        let mut sides = sides.iter();
+        let h = sides.next().unwrap();
+        let w = sides.next().unwrap();
+        let l = sides.next().unwrap();
+        amount_of_paper += 2*l*w + 2*l*h + 3*w*h;
+    }
+
+    println!("--- Part 1 ---");
+    println!("paper: {}", amount_of_paper);
+}
+
+fn part_2(input: &Vec<Vec<u32>>) {
+    let mut amount_of_ribbon = 0 as u32;
+
+    for present in input {
+        let mut sides = present.clone();
+        sides.sort();
+        let mut sides = sides.iter();
+        let h = sides.next().unwrap();
+        let w = sides.next().unwrap();
+        let l = sides.next().unwrap();
+        amount_of_ribbon += 2*(w+h) + l*w*h;
+    }
+
+    println!("--- Part 2 ---");
+    println!("ribbon: {}", amount_of_ribbon);
+}
 fn main() {
     let input = fs::read_to_string("input.txt")
         .unwrap()
@@ -11,23 +44,6 @@ fn main() {
         })
         .collect::<Vec<Vec<u32>>>();
 
-    let mut amount_of_paper = 0 as u32;
-    let mut amount_of_ribbon = 0 as u32;
-
-    for present in input {
-        let mut sides = present.clone();
-        sides.sort();
-        let mut sides = sides.iter();
-        let h = sides.next().unwrap();
-        let w = sides.next().unwrap();
-        let l = sides.next().unwrap();
-        amount_of_paper += 2*l*w + 2*l*h + 3*w*h;
-        amount_of_ribbon += (1+l) * (w*h);
-    }
-
-    println!("--- Part 1 ---");
-    println!("paper: {}", amount_of_paper);
-
-    println!("--- Part 2 ---");
-    println!("ribbon: {}", amount_of_ribbon);
+    part_1(&input);
+    part_2(&input);
 }
