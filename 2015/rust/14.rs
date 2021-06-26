38c45f5fa922613856bca1ff581647a12b73ec11
Cameron Fyfe <cameron.j.fyfe@gmail.com>
2021-06-26 17:37:30 -0600

    restructure rust projects to single file projects

---
+++ 2015/rust/14.rs
@@ -0,0 +1,129 @@
+// scan_fmt = "0.2"
+
+#[macro_use]
+extern crate scan_fmt;
+use std::collections::HashMap;
+use std::{env, fs};
+
+struct Reindeer {
+    speed: u32,    // km/s
+    duration: u32, // seconds
+    rest: u32,     // seconds
+}
+
+impl Reindeer {
+    fn distance(&self, t: &u32) -> u32 {
+        let (v, t_d, t_cycle) = (self.speed, self.duration, self.duration + self.rest);
+        v * ((t_d * (t / t_cycle)) + std::cmp::min(t_d, t % t_cycle))
+    }
+}
+
+fn reindeer_from_line(line: &str) -> Reindeer {
+    let (_, speed, duration, rest) = scan_fmt!(
+        line,
+        "{} can fly {} km/s for {} seconds, but then must rest for {} seconds.",
+        String,
+        u32,
+        u32,
+        u32
+    )
+    .expect("Error parsing reinder line.");
+    Reindeer {
+        speed,
+        duration,
+        rest,
+    }
+}
+
+fn part_1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| reindeer_from_line(line).distance(&2503))
+        .max()
+        .expect("Error: no max found.")
+}
+
+fn santas_scoring(time: u32, reindeer: Vec<Reindeer>) -> u32 {
+    let mut score = HashMap::new();
+
+    // Determine winners at the end of each second and add 1 to each of their score
+    for t in 1..=time {
+        let positions = reindeer.iter().enumerate().map(|r| (r.0, r.1.distance(&t)));
+        let leading_position = positions
+            .clone()
+            .map(|p| p.1)
+            .max()
+            .expect("Error: max not found.");
+        let winners = positions.filter(|p| p.1 == leading_position);
+        for winner in winners {
+            *score.entry(winner.0).or_insert(0) += 1;
+        }
+    }
+
+    *score.values().max().expect("Error: max not found.")
+}
+
+fn part_2(input: &str) -> u32 {
+    santas_scoring(
+        2503,
+        input
+            .lines()
+            .map(|line| reindeer_from_line(line))
+            .collect::<Vec<Reindeer>>(),
+    )
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let input = fs::read_to_string(&args[1]).expect("Error: can't read input file.");
+
+    println!("--- Part 1 ---");
+    println!("winner's distance: {}", part_1(&input)); // 2640
+
+    println!("--- Part 2 ---");
+    println!("winner's score: {}", part_2(&input)); // 998
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn comet() -> Reindeer {
+        reindeer_from_line(
+            "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+        )
+    }
+    fn dancer() -> Reindeer {
+        reindeer_from_line(
+            "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+        )
+    }
+    #[test]
+    fn part_1_example_1() {
+        assert_eq!(comet().distance(&1), 14);
+        assert_eq!(dancer().distance(&1), 16);
+    }
+    #[test]
+    fn part_1_example_2() {
+        assert_eq!(comet().distance(&10), 140);
+        assert_eq!(dancer().distance(&10), 160);
+    }
+    #[test]
+    fn part_1_example_3() {
+        assert_eq!(comet().distance(&11), 140);
+        assert_eq!(dancer().distance(&11), 176);
+    }
+    #[test]
+    fn part_1_example_4() {
+        assert_eq!(comet().distance(&12), 140);
+        assert_eq!(dancer().distance(&12), 176);
+    }
+    #[test]
+    fn part_1_example_5() {
+        assert_eq!(comet().distance(&1000), 1120);
+        assert_eq!(dancer().distance(&1000), 1056);
+    }
+    #[test]
+    fn part_2_example_1() {
+        assert_eq!(santas_scoring(1000, vec![comet(), dancer()]), 689);
+    }
+}
---
+++ 2015/rust/16.rs
@@ -0,0 +1,97 @@
+// scan_fmt = "0.2"
+
+#[macro_use]
+extern crate scan_fmt;
+use std::collections::HashMap;
+use std::{env, fs};
+
+struct Sue {
+    id: usize,
+    properties: HashMap<String, u32>,
+}
+
+impl Sue {
+    fn from_line(line: &str) -> Sue {
+        let first_sc = line
+            .chars()
+            .position(|c| c == ':')
+            .expect("Error parsing line.");
+        let id = scan_fmt!(&line[..first_sc], "Sue {}", usize)
+            .expect("Error parsing line with Sue info.");
+        let mut properties = HashMap::new();
+        for item in line[first_sc + 2..].split(", ") {
+            let (key, value) =
+                scan_fmt!(item, "{}: {}", String, u32).expect("Error parsing property on line.");
+            properties.insert(key, value);
+        }
+        Sue { id, properties }
+    }
+}
+
+fn ticker_tape() -> HashMap<String, u32> {
+    let mut tape = HashMap::new();
+    for p in vec![
+        ("children", 3 as u32),
+        ("cats", 7),
+        ("samoyeds", 2),
+        ("pomeranians", 3),
+        ("akitas", 0),
+        ("vizslas", 0),
+        ("goldfish", 5),
+        ("trees", 3),
+        ("cars", 2),
+        ("perfumes", 1),
+    ]
+    .iter()
+    {
+        tape.insert(p.0.to_string(), p.1);
+    }
+    tape
+}
+
+fn parts_1_and_2(input: &str, part_2: bool) -> Option<usize> {
+    let sues = input
+        .lines()
+        .map(|line| Sue::from_line(line))
+        .collect::<Vec<Sue>>();
+    let tape = ticker_tape();
+
+    match sues.iter().find(|sue| {
+        // True if all properties match ticker tape
+        sue.properties
+            .iter()
+            .map(|p| match tape.get(p.0) {
+                Some(val) => if part_2 {
+                    match p.0.as_str() {
+                        "cats" | "trees" => (p.1 > val),
+                        "pomeranians" | "goldfish" => (p.1 < val),
+                        _ => (p.1 == val),
+                    }
+                } else {
+                    p.1 == val
+                }
+                None => false,
+            })
+            .any(|b| b == false) == false
+    }) {
+        Some(sue) => Some(sue.id),
+        None => None,
+    }
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let input = fs::read_to_string(&args[1]).expect("Error reading input file.");
+
+    println!("--- Part 1 ---");
+    println!(
+        "Sue number: {}",
+        parts_1_and_2(&input, false).expect("Couldn't find Sue match.")
+    );
+
+    println!("--- Part 2 ---");
+    println!(
+        "Sue number: {}",
+        parts_1_and_2(&input, true).expect("Couldn't find Sue match.")
+    );
+}
